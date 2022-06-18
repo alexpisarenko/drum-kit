6ea21b8ac0d20a6c5490e6212ba9fa8345b804b4
Alex <me@alexpisarenko.com>
2022-06-18 00:18:58 -0400

    modified sound objects

--- index.js
+++ index.js
@@ -16,34 +16,37 @@ for (var i = 0; i < numOfButtons; i++){
 
         var buttonInnerHTML = this.innerHTML;
 
+        var crash = new Audio("sounds/crash.mp3");
+        var kickBass = new Audio("sounds/kick-bass.mp3");
+        var snare = new Audio("sounds/snare.mp3");
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        var tom4 = new Audio("sounds/tom-4.mp3");
+
+
         switch(buttonInnerHTML) {
             case "w":
-                    var sound = new Audio("sounds/crash.mp3");
-                    sound.play();
+                    crash.play();
                 break;
             case "a":
-                    var sound = new Audio("sounds/kick-bass.mp3");
-                    sound.play();
+                    kickBass.play();
                 break;
              case "s":
-                    var sound = new Audio("sounds/snare.mp3");
-                    sound.play();
+                    snare.play();
                 break;
             case "d":
-                    var sound = new Audio("sounds/tom-1.mp3");
-                    sound.play();
+                    
+                    tom1.play();
                 break;
             case "j":
-                    var sound = new Audio("sounds/tom-2.mp3");
-                    sound.play();
+                    tom2.play()
                 break;
                 case "k":
-                    var sound = new Audio("sounds/tom-3.mp3");
-                    sound.play();
+                    tom3.play()
                 break;
             case "l":
-                    var sound = new Audio("sounds/tom-4.mp3");
-                    sound.play();
+                    tom4.play();
                 break;
 
             default:
